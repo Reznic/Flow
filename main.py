df61fa731af0bca070a6828886a97f0bacdd3728
Reznic <reznic.tal@gmail.com>
2022-02-26 09:12:26 +0200

    tcp session timeout now working

---
+++ main.py
@@ -0,0 +1,19 @@
+from scapy.all import *
+from net_scanner import NetScanner
+
+
+INPUT_PCAP = 'net.pcap'
+
+
+def main():
+    scanner = NetScanner()
+    packets = rdpcap(INPUT_PCAP)
+    for pkt_num, packet in enumerate(packets):
+        scanner.parse_packet(packet, pkt_num)
+
+    scanner.connections_graph.print_table()
+
+
+if __name__ == "__main__":
+    main()
+
